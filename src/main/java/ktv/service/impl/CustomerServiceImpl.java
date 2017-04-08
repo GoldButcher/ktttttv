da55264a264010ac86fbe2dbfa3f418b9db19855
GoldButcher <947538570@qq.com>
2017-04-09 02:58:57 +0800

    wxEnd login html/js/java

--- src/main/java/ktv/service/impl/CustomerServiceImpl.java
+++ src/main/java/ktv/service/impl/CustomerServiceImpl.java
@@ -2,13 +2,17 @@ package ktv.service.impl;
 
 import ktv.dao.AbstractDao;
 import ktv.dao.ICustomerDao;
+import ktv.dto.CommonResponseDto;
 import ktv.dto.CustomerInfoDto;
 import ktv.model.Customer;
 import ktv.service.ICustomerService;
+import ktv.util.CheckUtil;
+import ktv.util.CommonResponseUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -28,5 +32,16 @@ public class CustomerServiceImpl extends AbstractServiceimpl<Integer,Customer> i
     @Override
     public List<CustomerInfoDto> getAllCustomerByDate(String date) {
         return customerDao.getAllCustomerByDate(date);
+    }
+
+    @Override
+    public CommonResponseDto getCheck(String mobile, HttpSession session) throws Exception {
+        CheckUtil.send(mobile,session);
+        return CommonResponseUtil.successWithNull();
+    }
+
+    @Override
+    public CommonResponseDto check(String str, HttpSession session) {
+        return CheckUtil.check(str,session);
     }
 }
---
+++ src/main/resources/static/wxEnd/login.js
@@ -0,0 +1,60 @@
+var InterValObj; //timer变量，控制时间
+var count = 60; //间隔函数，1秒执行
+var curCount;//当前剩余秒数
+
+function SetRemainTime() {
+    if (curCount == 0) {
+        window.clearInterval(InterValObj);// 停止计时器
+        $("#getCheck").removeAttr("disabled");// 启用按钮
+        $("#getCheck").html("获取验证码");
+    }else {
+        curCount--;
+        $("#getCheck").html("在"+curCount + "秒内输入");
+    }
+}
+
+$("#getCheck").click(function () {
+    curCount = count;
+    var mobile = $(".mobile").val();
+    $("#getCheck").attr("disabled", "true");
+    $("#getCheck").html("在"+curCount + "秒内输入");
+    InterValObj = window.setInterval(SetRemainTime, 1000); // 启动计时器，1秒执行一次
+    // $.ajax({
+    //     url:"/getCheck",
+    //     type:"GET",
+    //     dataType:"JSON",
+    //     data :{"mobile":mobile},
+    //     success:function(data){
+    //         //alert("成功发送");
+    //     },
+    //     error : function(data){
+    //     }
+    // });
+})
+
+$(".checkCode").keydown(function () {
+    var val= $(this).val();
+    if(val.trim().length>=5){
+        $("#logBtn").prop("disabled",false);
+    }else {
+        $("#logBtn").prop("disabled", true)
+    }
+})
+
+$("#logBtn").click(function () {
+    var checkCode = $(".checkCode").val();
+    $.ajax({
+        url:"/check",
+        type:"GET",
+        dataType:"JSON",
+        data:{"checkCode":checkCode},
+        success:function (msg) {
+            if(msg.result=="error"){
+                alert(msg.data);
+            }else{
+                sessionStorage.mobile = $(".mobile").val();
+                window.location.href="index.html";
+            }
+        }
+    })
+})
