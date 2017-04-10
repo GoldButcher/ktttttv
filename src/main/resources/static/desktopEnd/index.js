2ae997d4a37111aef8549903ed2ce093e86b0b74
GoldButcher <947538570@qq.com>
2017-04-10 15:51:15 +0800

    wxEnd index.js

---
+++ src/main/resources/static/desktopEnd/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2017/4/10.
+ */
+$(function () {
+    function GetQueryString(name)
+    {
+        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if(r!=null)return  unescape(r[2]); return null;
+    }
+
+    alert(GetQueryString("code"));
+
+})
