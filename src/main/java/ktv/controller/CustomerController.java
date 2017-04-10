1716d26d09612a55f2752910c2865cec14ebc428
GoldButcher <947538570@qq.com>
2017-04-10 19:08:26 +0800

    wx get user openid

--- src/main/java/ktv/controller/CustomerController.java
+++ src/main/java/ktv/controller/CustomerController.java
@@ -12,7 +12,9 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -48,30 +50,46 @@ public class CustomerController {
         return customerService.getAllCustomerByDate(date);
     }
 
+//    /**
+//     * @title:用户获取验证码
+//     * @user:admin
+//     * @return:CommonResponseDto
+//     * @date:2017/4/9 14:59
+//     **/
+//    @RequestMapping(value = "/getCheck",method = RequestMethod.GET)
+//    public CommonResponseDto getCheck(String mobile,HttpSession session){
+//        try {
+//            return customerService.getCheck(mobile, session);
+//        } catch (Exception e) {
+//            return CommonResponseUtil.errorWithObj("网络错误,请重新获取");
+//        }
+//    }
+//
+//    /**
+//     * @title:验证码验证函数
+//     * @user:admin
+//     * @return:CommonResponseDto
+//     * @date:2017/4/9 14:59
+//     **/
+//    @RequestMapping(value = "check",method = RequestMethod.GET)
+//    public CommonResponseDto check(String checkCode,HttpSession session){
+//        return customerService.check(checkCode,session);
+//    }
     /**
-     * @title:用户获取验证码
-     * @user:admin
-     * @return:CommonResponseDto
-     * @date:2017/4/9 14:59
+          * @title:客户登陆
+          * @user:admin
+          * @return:ModelAndView
+          * @date:2017/4/9 14:59
      **/
-    @RequestMapping(value = "/getCheck",method = RequestMethod.GET)
-    public CommonResponseDto getCheck(String mobile,HttpSession session){
-        try {
-            return customerService.getCheck(mobile, session);
-        } catch (Exception e) {
-            return CommonResponseUtil.errorWithObj("网络错误,请重新获取");
-        }
+    @RequestMapping(value = "/customerLogin")
+    public ModelAndView customerLogin(HttpServletRequest request){
+        return customerService.customerLogin(request);
     }
 
-    /**
-     * @title:验证码验证函数
-     * @user:admin
-     * @return:CommonResponseDto
-     * @date:2017/4/9 14:59
-     **/
-    @RequestMapping(value = "check",method = RequestMethod.GET)
-    public CommonResponseDto check(String checkCode,HttpSession session){
-        return customerService.check(checkCode,session);
+    @RequestMapping(value = "/getOpenId",method = RequestMethod.GET)
+    public CommonResponseDto getOpendId(HttpSession session){
+        String openid = (String) session.getAttribute("openid");
+        System.out.println(openid);
+        return CommonResponseUtil.successWithObj(openid);
     }
-
 }
--- src/main/resources/static/wxEnd/index.js
+++ src/main/resources/static/wxEnd/index.js
@@ -2,13 +2,12 @@
  * Created by admin on 2017/4/10.
  */
 $(function () {
-    function GetQueryString(name)
-    {
-        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(reg);
-        if(r!=null)return  unescape(r[2]); return null;
-    }
-
-    alert(GetQueryString("code"));
+    $.ajax({
+        "url":"/getOpenId",
+        "type":"GET",
+        success:function (msg) {
+            alert(msg.data);
+        }
+    })
 
 })
