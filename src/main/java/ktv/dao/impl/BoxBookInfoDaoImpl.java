3a671eec4fd00507a9a62d59a92a0e16097e06a9
yingchaoyong <yingchaoyong@gmail.com>
2017-04-12 05:51:04 +0800

    cx

--- src/main/java/ktv/dao/impl/BoxBookInfoDaoImpl.java
+++ src/main/java/ktv/dao/impl/BoxBookInfoDaoImpl.java
@@ -18,33 +18,33 @@ public class BoxBookInfoDaoImpl extends AbstractDaoImpl<Integer, BoxBookInfo> im
 
     @Override
     public BoxBookInfo getOrder(String boxNo, String date) {
-        String hql = "select bb from  BoxInfo bf ,BoxBookInfo bb where bf.boxInfoId = bb.boxInfoId "+
+        String hql = "select bb from  BoxInfo bf ,BoxBookInfo bb where bf.boxInfoId = bb.boxInfoId " +
                 " and bf.boxNo=:boxNo and bb.orderDate=:orderDate";
         Query query = getSession().createQuery(hql);
-        query.setString("boxNo",boxNo).setString("orderDate",date);
+        query.setString("boxNo", boxNo).setString("orderDate", date);
         return (BoxBookInfo) query.uniqueResult();
     }
 
     @Override
     public PersonOrderDto getBooxInfo4Order(BoxBookInfo boxBookInfo) {
         String sql = "SELECT bb.orderDate AS orderDate,bt.name AS boxTypeName,bf.price AS price, bf.boxNo AS boxNo"
-                +" ,cs.telephone AS telephone "
-                +"FROM BoxBookInfo bb , BoxInfo bf,Customer cs,BoxType bt WHERE"
-                +" bb.boxInfoId = bf.boxInfoId and bf.boxTypeId = bt.boxTypeId AND bb.customerId = cs.customerId AND"
-                +" bb.orderDate = :orderDate AND bb.boxInfoId = :boxInfoId";
+                + " ,cs.telephone AS telephone "
+                + "FROM BoxBookInfo bb , BoxInfo bf,Customer cs,BoxType bt WHERE"
+                + " bb.boxInfoId = bf.boxInfoId and bf.boxTypeId = bt.boxTypeId AND bb.customerId = cs.customerId AND"
+                + " bb.orderDate = :orderDate AND bb.boxInfoId = :boxInfoId";
         Query query = getSession().createSQLQuery(sql).setResultTransformer(Transformers.aliasToBean(PersonOrderDto.class));
-        query.setString("orderDate",boxBookInfo.getOrderDate()).setInteger("boxInfoId",boxBookInfo.getBoxInfoId());
+        query.setString("orderDate", boxBookInfo.getOrderDate()).setInteger("boxInfoId", boxBookInfo.getBoxInfoId());
         return (PersonOrderDto) query.uniqueResult();
     }
 
     @Override
     public List<PersonOrderDto> getBookInfoFromWeixin(String weixin) {
         String sql = "SELECT bb.orderDate AS orderDate,bt.name AS boxTypeName,bf.price AS price, bf.boxNo AS boxNo"
-                +" ,cs.telephone AS telephone"
-                +"FROM BoxBookInfo bb , BoxInfo bf,Customer cs,BoxType bt WHERE"
-                +" bb.boxInfoId = bf.boxInfoId and bf.boxTypeId = bt.boxTypeId AND bb.customerId = cs.customerId and cs.weixin =:weixin ORDER BY bb.orderDate DESC ";
+                + " ,cs.telephone AS telephone"
+                + "FROM BoxBookInfo bb , BoxInfo bf,Customer cs,BoxType bt WHERE"
+                + " bb.boxInfoId = bf.boxInfoId and bf.boxTypeId = bt.boxTypeId AND bb.customerId = cs.customerId and cs.weixin =:weixin ORDER BY bb.orderDate DESC ";
         Query query = getSession().createSQLQuery(sql).setResultTransformer(Transformers.aliasToBean(PersonOrderDto.class));
-        query.setString("weixin",weixin);
+        query.setString("weixin", weixin);
         return query.list();
     }
 }
--- src/main/java/ktv/service/impl/BoxBookInfoServiceImpl.java
+++ src/main/java/ktv/service/impl/BoxBookInfoServiceImpl.java
@@ -18,13 +18,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by admin on 2016/12/25.
  */
 @Transactional
 @Service
-public class BoxBookInfoServiceImpl extends AbstractServiceimpl<Integer,BoxBookInfo> implements IBoxBookInfoService{
+public class BoxBookInfoServiceImpl extends AbstractServiceimpl<Integer, BoxBookInfo> implements IBoxBookInfoService {
     @Resource
     private IBoxBookInfoDao boxBookInfoDao;
 
@@ -33,29 +34,29 @@ public class BoxBookInfoServiceImpl extends AbstractServiceimpl<Integer,BoxBookI
 
     @Resource
     private ICustomerDao customerDao;
+
     @Resource
     public void setBoxBookInfoDao(IBoxBookInfoDao boxBookInfoDao) {
         super.setAbstractDao(boxBookInfoDao);
     }
 
     @Override
-    public CommonResponseDto bookBox(String date, String boxNo,String weixin,String telephone,int payment,String username,String description) {
+    public CommonResponseDto bookBox(String date, String boxNo, String weixin, String telephone, int payment, String username, String description) {
 
-        BoxInfo boxInfo = boxInfoDao.getByProperties("boxNo",boxNo);
-        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo,date);
-        if(boxBookInfo != null) return CommonResponseUtil.errorWithObj("该包厢已经被预定！");
-        Customer customer = customerDao.getByProperties("telephone",telephone);
+        BoxInfo boxInfo = boxInfoDao.getByProperties("boxNo", boxNo);
+        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo, date);
+        if (boxBookInfo != null) return CommonResponseUtil.errorWithObj("该包厢已经被预定！");
+        Customer customer = customerDao.getByProperties("telephone", telephone);
         int customerId = 0;
-        if(customer==null)
-        {
+        if (customer == null) {
             customer = new Customer();
             customer.setTelephone(telephone);
-            if(weixin!="")
-            customer.setWeixin(weixin);
+            if (!Objects.equals(weixin, ""))
+                customer.setWeixin(weixin);
             customerId = customerDao.sava(customer);
-        }else{
-            if(weixin!="")
-            customer.setWeixin(weixin);
+        } else {
+            if (!Objects.equals(weixin, ""))
+                customer.setWeixin(weixin);
             customerId = customer.getCustomerId();
             customerDao.update(customer);
         }
@@ -67,17 +68,17 @@ public class BoxBookInfoServiceImpl extends AbstractServiceimpl<Integer,BoxBookI
         boxBookInfo.setPayment(payment);
         boxBookInfo.setUserName(username);
         boxBookInfo.setDescription(description);
-        if(0!=boxBookInfoDao.sava(boxBookInfo))
+        if (0 != boxBookInfoDao.sava(boxBookInfo))
             return CommonResponseUtil.successWithNull();
         return CommonResponseUtil.errorWithObj("系统错误,请稍后再试");
     }
 
     @Override
     public CommonResponseDto cancelBookBox(String date, String boxNo, String telephone) {
-        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo,date);
-        if(boxBookInfo==null) return CommonResponseUtil.errorWithObj("请确认该订单是否存在!");
+        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo, date);
+        if (boxBookInfo == null) return CommonResponseUtil.errorWithObj("请确认该订单是否存在!");
         Customer customer = customerDao.getByKey(boxBookInfo.getCustomerId());
-        if(!customer.getTelephone().equals(telephone.trim())) return CommonResponseUtil.errorWithObj("请填写正确的预留手机号!");
+        if (!customer.getTelephone().equals(telephone.trim())) return CommonResponseUtil.errorWithObj("请填写正确的预留手机号!");
 
         boxBookInfoDao.delete(boxBookInfo);
         return CommonResponseUtil.successWithNull();
@@ -89,9 +90,9 @@ public class BoxBookInfoServiceImpl extends AbstractServiceimpl<Integer,BoxBookI
     }
 
     @Override
-    public CommonResponseDto payOrder(String date, String boxNo,int payment) {
-        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo,date);
-        if(boxBookInfo == null)
+    public CommonResponseDto payOrder(String date, String boxNo, int payment) {
+        BoxBookInfo boxBookInfo = boxBookInfoDao.getOrder(boxNo, date);
+        if (boxBookInfo == null)
             return CommonResponseUtil.errorWithObj("支付失败请稍后再试！");
         boxBookInfo.setPayment(payment);
         boxBookInfoDao.update(boxBookInfo);
