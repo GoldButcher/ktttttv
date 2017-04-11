25b57e27fc42885ca8ebdd671fe694e6fdcbe5c5
GoldButcher <947538570@qq.com>
2017-04-11 15:10:37 +0800

    weixin order

--- src/main/resources/static/wxEnd/index.js
+++ src/main/resources/static/wxEnd/index.js
@@ -6,8 +6,35 @@ $(function () {
         "url":"/getOpenId",
         "type":"GET",
         success:function (msg) {
-            alert(msg.data);
+            sessionStorage.setItem("openid",msg.data);
         }
     })
+    $(".history").on('click', function () {
+
+        location.href = 'history.html';
+    });
+    $(".boxDetail").on('click', function () {
+        sessionStorage.setItem ("typeId" ,$(this).data("typeId"));
+        sessionStorage.setItem ("price" ,$(this).data("price"));
+        sessionStorage.setItem ("name" ,$(this).data("name"));
+        location.href = 'boxDetail.html';
+    })
+    $.ajax({
+        "url":"/getAllBoxTypeInfo",
+        "type":"GET",
+        "success":function(data){
+            var accinfo = $(".acc-info");
+            for(var i=0;i<accinfo.length;i++){
+                var span1 = $(accinfo).eq(i).find("span").eq(0);
+                var span2 = $(accinfo).eq(i).find("span").eq(1);
+                $(span1).text(data.data[i].name);
+                $(span2).text("￥"+data.data[i].price);
+                $(".boxDetail").eq(i).data("typeId",data.data[i].boxTypeId);
+                $(".boxDetail").eq(i).data("price",data.data[i].price);
+                $(".boxDetail").eq(i).data("name",data.data[i].name);
+            }
+
+        }
+    });
 
 })
